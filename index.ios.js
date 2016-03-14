95662af1aa5456fcb372f84002e225edd8bfe4bb
John Hwang <johnsies87@gmail.com>
2016-03-14 00:03:42 -0700

    starting with fresh index page

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,8 @@
 // this loads react native module, and assigns it to React variable.
 var React = require('react-native');
 
+
+// adding css
 var styles = React.StyleSheet.create({
   text: {
     color: 'black',
