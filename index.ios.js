69eec097e0078f87af8836bb27f7c9cfa5f8da2c
John Hwang <johnsies87@gmail.com>
2016-03-13 23:53:14 -0700

    first commit

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+
+'use strict';
+
+// this loads react native module, and assigns it to React variable.
+var React = require('react-native');
+
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
